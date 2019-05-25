91df7aa24c35b377d298b496e03bc051189b6eb3
steveri <steveri@steveri.com>
2019-05-25 15:18:39 -0700

    srsub 4 - 1 = 3 woohoo

--- rtl/lib/srsub.v
+++ rtl/lib/srsub.v
@@ -34,17 +34,13 @@ module srsub(
   wire [7:0] ediff; assign ediff = (ae >= be) ? (ae - be) : (be - ae);
 
 
-
-
-  // Shift the larger operand left ediff bits, and reduce exp accordingly
-
+  // Shift the larger operand left 'ediff' bits, and reduce exp accordingly
   wire [47:0] am_adj; assign am_adj = (ediff > 8'd22) ? am : (
     (ae > be) ? (am << ediff) : am
   ); 
   wire [47:0] bm_adj; assign bm_adj = (ediff > 8'd22) ? bm : (
     (be > ae) ? (bm << ediff) : bm
   ); 
-
 
 /*
   // FIXME check to see if exp will go l.t. zero
@@ -64,52 +60,99 @@ module srsub(
   wire [47:0] abs_am_minus_bm;
   assign abs_am_minus_bm = (am_adj >= bm_adj) ? (am_adj - bm_adj) : (bm_adj - am_adj);
 
+  // Previously, we normalized by shifting larger operand *left* by 'ediff' bits
+  // Now we can shift back down (remember, ediff can be as small as 0 bits)
+
   // use bit range 23+ediff down to ediff, right?
   wire [23:0] zm; assign zm = (abs_am_minus_bm >> ediff);
 
-  wire [22:0] zm_final; assign zm_final = 
-/*
-    zm[23] == 1'b1 ? {       zm[22:0]} :
-    zm[22] == 1'b1 ? { 1'b0, zm[21:0]} :
-    zm[21] == 1'b1 ? { 2'b0, zm[20:0]} :
-    zm[20] == 1'b1 ? { 3'b0, zm[19:0]} :
-*/
+// Huh this whole thing seems wrong
+//   wire [22:0] zm_final; assign zm_final = 
+//     zm[23+ediff] == 1'b1 ? zm >> ediff :
+//     zm[22+ediff] == 1'b1 ? zm >> (ediff+1) :
+//     zm[21+ediff] == 1'b1 ? zm >> (ediff+2) : <<<<<<< ediff=1 zm=4ffff zm[22], >> 3
+//     zm[20+ediff] == 1'b1 ? zm >> (ediff+3) :
+// /*
+//     zm[22+ediff] == 1'b1 ? zm[21+ediff:ediff] :
+//     zm[21+ediff] == 1'b1 ? zm[20+ediff:ediff] :
+//     zm[20+ediff] == 1'b1 ? zm[19+ediff:ediff] :
+// */
+// 
+//     zm[19] == 1'b1 ? { 4'b0, zm[18:0]} :
+//     zm[18] == 1'b1 ? { 5'b0, zm[17:0]} :
+//     zm[17] == 1'b1 ? { 6'b0, zm[16:0]} :
+//     zm[16] == 1'b1 ? { 7'b0, zm[15:0]} :
+//     zm[15] == 1'b1 ? { 8'b0, zm[14:0]} :
+//     zm[14] == 1'b1 ? { 9'b0, zm[13:0]} :
+//     zm[13] == 1'b1 ? {10'b0, zm[12:0]} :
+//     zm[12] == 1'b1 ? {11'b0, zm[11:0]} :
+//     zm[11] == 1'b1 ? {12'b0, zm[10:0]} :
+//     zm[10] == 1'b1 ? {13'b0, zm[ 9:0]} :
+// 
+//     zm[9] == 1'b1 ? {14'b0, zm[8:0]} :
+//     zm[8] == 1'b1 ? {15'b0, zm[7:0]} :
+//     zm[7] == 1'b1 ? {16'b0, zm[6:0]} :
+//     zm[6] == 1'b1 ? {17'b0, zm[5:0]} :
+//     zm[5] == 1'b1 ? {18'b0, zm[4:0]} :
+//     zm[4] == 1'b1 ? {19'b0, zm[3:0]} :
+//     zm[3] == 1'b1 ? {20'b0, zm[2:0]} :
+//     zm[2] == 1'b1 ? {21'b0, zm[1:0]} :
+//     zm[1] == 1'b1 ? {22'b0, zm[0:0]} :
+//                     {23'b0};
+// /*
+//     zm[19+ediff] == 1'b1 ? zm >> (ediff+4) :
+//     zm[18+ediff] == 1'b1 ? zm >> (ediff+5) :
+//     zm[17+ediff] == 1'b1 ? zm >> (ediff+6) :
+//     zm[16+ediff] == 1'b1 ? zm >> (ediff+7) :
+//     zm[15+ediff] == 1'b1 ? zm >> (ediff+8) :
+//     zm[14+ediff] == 1'b1 ? zm >> (ediff+9) :
+//     zm[13+ediff] == 1'b1 ? zm >> (ediff+10) :
+//     zm[12+ediff] == 1'b1 ? zm >> (ediff+11) :
+//     zm[11+ediff] == 1'b1 ? zm >> (ediff+12) :
+//     zm[10+ediff] == 1'b1 ? zm >> (ediff+13) :
+// 
+//     zm[9+ediff] == 1'b1 ? zm >> (ediff+14) :
+//     zm[8+ediff] == 1'b1 ? zm >> (ediff+15) :
+//     zm[7+ediff] == 1'b1 ? zm >> (ediff+16) :
+//     zm[6+ediff] == 1'b1 ? zm >> (ediff+17) :
+//     zm[5+ediff] == 1'b1 ? zm >> (ediff+18) :
+//     zm[4+ediff] == 1'b1 ? zm >> (ediff+19) :
+//     zm[3+ediff] == 1'b1 ? zm >> (ediff+20) :
+//     zm[2+ediff] == 1'b1 ? zm >> (ediff+21) :
+//     zm[1+ediff] == 1'b1 ? zm >> (ediff+22) :
+//     23'b0;
+// */
 
-//    zm[23+ediff] == 1'b1 ? zm[22+ediff:ediff] :
-    zm[23+ediff] == 1'b1 ? zm >> ediff :
-    zm[22+ediff] == 1'b1 ? zm >> (ediff+1) :
-    zm[21+ediff] == 1'b1 ? zm >> (ediff+2) :
-    zm[20+ediff] == 1'b1 ? zm >> (ediff+3) :
+  // Top bit should be '1' for normalization (one gets hidden later)
+  wire [7:0] need_shift; assign need_shift = 
+    zm[23] == 1'b1 ?  0 :
+    zm[22] == 1'b1 ?  1 :
+    zm[21] == 1'b1 ?  2 :
+    zm[20] == 1'b1 ?  3 :
+    zm[19] == 1'b1 ?  4 :
 
+    zm[18] == 1'b1 ?  5 :
+    zm[17] == 1'b1 ?  6 :
+    zm[16] == 1'b1 ?  7 :
+    zm[15] == 1'b1 ?  8 :
+    zm[14] == 1'b1 ?  9 :
+    zm[13] == 1'b1 ? 10 :
+    zm[12] == 1'b1 ? 11 :
+    zm[11] == 1'b1 ? 12 :
+    zm[10] == 1'b1 ? 13 :
+    zm[ 9] == 1'b1 ? 14 :
+    zm[ 8] == 1'b1 ? 15 :
+    zm[ 7] == 1'b1 ? 16 :
+    zm[ 6] == 1'b1 ? 17 :
+    zm[ 5] == 1'b1 ? 18 :
+    zm[ 4] == 1'b1 ? 19 :
+    zm[ 3] == 1'b1 ? 20 :
+    zm[ 2] == 1'b1 ? 21 :
+    zm[ 1] == 1'b1 ? 22 :
+    zm[ 0] == 1'b1 ? 23 : 24;
 
-/*
-    zm[22+ediff] == 1'b1 ? zm[21+ediff:ediff] :
-    zm[21+ediff] == 1'b1 ? zm[20+ediff:ediff] :
-    zm[20+ediff] == 1'b1 ? zm[19+ediff:ediff] :
-*/
+  wire [22:0] zm_final; assign zm_final = zm << need_shift;
 
-
-    zm[19] == 1'b1 ? { 4'b0, zm[18:0]} :
-    zm[18] == 1'b1 ? { 5'b0, zm[17:0]} :
-    zm[17] == 1'b1 ? { 6'b0, zm[16:0]} :
-    zm[16] == 1'b1 ? { 7'b0, zm[15:0]} :
-    zm[15] == 1'b1 ? { 8'b0, zm[14:0]} :
-    zm[14] == 1'b1 ? { 9'b0, zm[13:0]} :
-    zm[13] == 1'b1 ? {10'b0, zm[12:0]} :
-    zm[12] == 1'b1 ? {11'b0, zm[11:0]} :
-    zm[11] == 1'b1 ? {12'b0, zm[10:0]} :
-    zm[10] == 1'b1 ? {13'b0, zm[ 9:0]} :
-
-    zm[9] == 1'b1 ? {14'b0, zm[8:0]} :
-    zm[8] == 1'b1 ? {15'b0, zm[7:0]} :
-    zm[7] == 1'b1 ? {16'b0, zm[6:0]} :
-    zm[6] == 1'b1 ? {17'b0, zm[5:0]} :
-    zm[5] == 1'b1 ? {18'b0, zm[4:0]} :
-    zm[4] == 1'b1 ? {19'b0, zm[3:0]} :
-    zm[3] == 1'b1 ? {20'b0, zm[2:0]} :
-    zm[2] == 1'b1 ? {21'b0, zm[1:0]} :
-    zm[1] == 1'b1 ? {22'b0, zm[0:0]} :
-                    {23'b0};
 
   // Remember, we adjusted the larger op to match the smaller...
   // wire [7:0] ze; assign ze = (ae < be)? ae : be;
@@ -119,37 +162,39 @@ module srsub(
   wire [7:0] ze; assign ze = (ae > be)? ae : be;
 
 
+// ae > be; ae=0x81; zm=0x600000; zm[22] = 1; want new ze = 0x80
 
-  wire [7:0] ze_finall; assign ze_finall =
-    zm[23] == 1'b1 ? ze + 7'd0 :
-    zm[22] == 1'b1 ? ze + 7'd1 :
-    zm[21] == 1'b1 ? ze + 7'd2 :
-    zm[20] == 1'b1 ? ze + 7'd3 :
+  wire [7:0] ze_final; assign ze_final =
+    zm[23] == 1'b1 ? ze - 7'd0 :
+    zm[22] == 1'b1 ? ze - 7'd1 :
+    zm[21] == 1'b1 ? ze - 7'd2 :
+    zm[20] == 1'b1 ? ze - 7'd3 :
 
-    zm[19] == 1'b1 ? ze + 7'd4 :
-    zm[18] == 1'b1 ? ze + 7'd5 :
-    zm[17] == 1'b1 ? ze + 7'd6 :
-    zm[16] == 1'b1 ? ze + 7'd7 :
-    zm[15] == 1'b1 ? ze + 7'd8 :
-    zm[14] == 1'b1 ? ze + 7'd9 :
-    zm[13] == 1'b1 ? ze + 7'd10 :
-    zm[12] == 1'b1 ? ze + 7'd11 :
-    zm[11] == 1'b1 ? ze + 7'd12 :
-    zm[10] == 1'b1 ? ze + 7'd13 :
+    zm[19] == 1'b1 ? ze - 7'd4 :
+    zm[18] == 1'b1 ? ze - 7'd5 :
+    zm[17] == 1'b1 ? ze - 7'd6 :
+    zm[16] == 1'b1 ? ze - 7'd7 :
+    zm[15] == 1'b1 ? ze - 7'd8 :
+    zm[14] == 1'b1 ? ze - 7'd9 :
+    zm[13] == 1'b1 ? ze - 7'd10 :
+    zm[12] == 1'b1 ? ze - 7'd11 :
+    zm[11] == 1'b1 ? ze - 7'd12 :
+    zm[10] == 1'b1 ? ze - 7'd13 :
 
-    zm[9] == 1'b1 ?  ze + 7'd14 :
-    zm[8] == 1'b1 ?  ze + 7'd15 :
-    zm[7] == 1'b1 ?  ze + 7'd16 :
-    zm[6] == 1'b1 ?  ze + 7'd17 :
-    zm[5] == 1'b1 ?  ze + 7'd18 :
-    zm[4] == 1'b1 ?  ze + 7'd19 :
-    zm[3] == 1'b1 ?  ze + 7'd20 :
-    zm[2] == 1'b1 ?  ze + 7'd21 :
-    zm[1] == 1'b1 ?  ze + 7'd22 :
-    zm[0] == 1'b1 ?  ze + 7'd23 :
+    zm[9] == 1'b1 ?  ze - 7'd14 :
+    zm[8] == 1'b1 ?  ze - 7'd15 :
+    zm[7] == 1'b1 ?  ze - 7'd16 :
+    zm[6] == 1'b1 ?  ze - 7'd17 :
+    zm[5] == 1'b1 ?  ze - 7'd18 :
+    zm[4] == 1'b1 ?  ze - 7'd19 :
+    zm[3] == 1'b1 ?  ze - 7'd20 :
+    zm[2] == 1'b1 ?  ze - 7'd21 :
+    zm[1] == 1'b1 ?  ze - 7'd22 :
+    zm[0] == 1'b1 ?  ze - 7'd23 :
     7'd0;
 
-  wire [7:0] ze_final; assign ze_final = ze_finall - 8'd1;
+//  // oops oh noooooo
+//  wire [7:0] ze_final; assign ze_final = ze_finall - 8'd1;
 
 
 //  // use if (as != bs)
@@ -161,20 +206,18 @@ module srsub(
   wire [31:0] a_plus_b;
   sradd SUBADDER (
 
-// `define TEST_SRADD_ONE_PLUS_ONE
-`define TEST_SRADD_ONE_PLUS_707
-
-`ifdef TEST_SRADD_ONE_PLUS_ONE
-    // FIXME TEST CODE
-    // 1 + 1 = 2?
-    .a(32'h3f800000),
-    .b(32'h3f800000),
-`else
-  `ifdef TEST_SRADD_ONE_PLUS_707
+//`define SRADD_TEST1
+`ifdef SRADD_TEST1
     // FIXME TEST CODE
     // 1 + .707 = 1.707?
     .a(32'h3f800000),
     .b(32'h3f3504f3),
+`else
+  `ifdef SRADD_TEST2
+    // FIXME TEST CODE
+    // 1 + 1 = 2?
+    .a(32'h3f800000),
+    .b(32'h3f800000),
   `else
     .a({a[`sign], a[`exponent], a[`mantissa]}),
     .b({a[`sign], b[`exponent], b[`mantissa]}),
@@ -183,7 +226,6 @@ module srsub(
 
     .z(a_plus_b)
   );
-
 
 
   ////////////////////////////////////////////////////////////////
@@ -205,9 +247,10 @@ module srsub(
   // DEBUGGING
   always @ (*) begin
 
-   if (a == 32'h3f800000) begin
-//    if ((a != 0) && (b != 0)) begin
-//    if ((a != 0) && (b == 32'h40000000)) begin
+//   if (a == 32'h3f800000) begin
+//   if ((a != 0) && (b != 0)) begin
+//   if ((a != 0) && (b == 32'h40000000)) begin
+   if (a == a) begin
   
       $display("srsub");
 
